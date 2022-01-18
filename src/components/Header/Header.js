214ff71a197e3b95b4a73c8eac18a755b16c62d8
awad erfani <jawad.erfani147@gmail.com>
2022-01-18 05:05:50 +0430

    Revert "Revert "Revert "header background color has been chenged"""
    This reverts commit da6dd0c23714a9494adc2b3a6e6209b204012a23.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ import {useGlobalContext } from '../../context'
 function Header() {
     const {Light, setLight} = useGlobalContext ()
     return (
-        <div className='flex justify-between  px-10 shadow-xl  pb-4 items-center  h-20 bg-slate-900 dark:bg-slate-600 text-gray-100'>
+        <div className='flex justify-between  px-10 shadow-xl  pb-4 items-center  h-20 bg-slate-800 dark:bg-slate-600 text-gray-100'>
             <div><h1 className='text-2xl   font-serif'>Country App</h1></div>      
             <div className='text-3xl cursor-pointer hover:scale-90' onClick={()=>setLight(!Light)}> {Light?<FaMoon/>: <FaSun/>}</div>
         </div>
